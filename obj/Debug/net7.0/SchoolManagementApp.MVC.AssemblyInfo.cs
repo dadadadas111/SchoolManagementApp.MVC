faf59a159e3772c11364ff8c1001dc51f17897b3
dadadadas111 <long6athcskl@gmail.com>
2025-03-29 15:43:34 +0700

    feat: migrate to .net8

--- obj/Debug/net7.0/SchoolManagementApp.MVC.AssemblyInfo.cs
+++ obj/Debug/net7.0/SchoolManagementApp.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementApp.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0738b17c00415035ba4ef6786593db5d969e3912")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ea04eb8efb4e55eb98d53102c9dadcb8e9b8c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementApp.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementApp.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
